f78cc2ef8f0ae2a36d4236d7182e25e66aab391d
Nikita <dedsec1612@gmail.com>
2020-08-07 08:02:27 +0300

    перезалив проекта, уже с JS

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
 "use strict";
 
-console.log('Test js!!!');
+var menuBurger = document.querySelector('.header__menu-burger');
+var header = document.querySelector('.header');
+var mainMenu = document.querySelector('.main-menu');
+var orderButton = document.querySelector('.button-js');
+var popup = document.querySelector('.callback-popup-wrapper');
+var closeButton = document.querySelector('.callback__close-button');
+/* Menu-burger */
+
+menuBurger.onclick = function () {
+  header.classList.toggle('header--active');
+  mainMenu.classList.toggle('main-menu--active');
+
+  if (header.classList.contains('header--active')) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "visible";
+  }
+};
+/* Popup */
+
+
+orderButton.onclick = function () {
+  popup.classList.add('callback-popup-wrapper--active');
+  document.body.style.overflow = "hidden";
+};
+
+closeButton.onclick = function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('callback-popup-wrapper--active');
+  document.body.style.overflow = "visible";
+};
 //# sourceMappingURL=index.js.map
